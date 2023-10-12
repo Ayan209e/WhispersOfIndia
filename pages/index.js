e00a91342381186a4152296a1facef736dd4864d
Anuj3366 <anujgarg3366@gmail.com>
2023-10-12 09:37:29 +0530

    naavbar trial

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Navbar from '../components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div></div>
+    <div>
+      <Navbar />
+    </div>
   )
 }
